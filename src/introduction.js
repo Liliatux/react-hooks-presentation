afb19cfc08b9c003dde4dc71d34d55e26ceef728
zgeerts <zelia.geerts@fullsave.com>
2019-11-16 18:58:17 +0100

    Add slides

---
+++ src/introduction.js
@@ -0,0 +1,80 @@
+// Import React
+import React from "react";
+
+// Import Spectacle Core tags
+import { BlockQuote, Cite, Deck, Heading, Slide, Text } from "spectacle";
+
+import CodeSlide from "spectacle-code-slide";
+
+// Import theme
+import createTheme from "spectacle/lib/themes/default";
+import WithoutHooks from "./WithoutHooks";
+import WithHooks from "./WithHooks";
+
+// Require CSS
+require("normalize.css");
+
+const theme = createTheme(
+  {
+    primary: "black",
+    secondary: "white",
+    tertiary: "#d347a7",
+    quarternary: "#CECECE"
+  },
+  {
+    primary: "Montserrat",
+    secondary: "Helvetica"
+  }
+);
+
+export default class Presentation extends React.Component {
+  render() {
+    console.log(require("raw-loader!./WithoutHooks"));
+    return (
+      <Deck
+        transition={["zoom"]}
+        transitionDuration={500}
+        theme={theme}
+        bgColor="primary"
+        contentWidth={1400}
+      >
+        <Slide transition={["zoom"]}>
+          <Heading textColor="secondary" size={4}>
+            React Hooks
+          </Heading>
+          <Text margin="10px 0 0" textColor="tertiary" bold>
+            React 16.8
+          </Text>
+          <br />
+          <BlockQuote>
+            <Cite textColor="secondary">@liliatux</Cite>
+          </BlockQuote>
+        </Slide>
+        <Slide transition={["zoom", "fade"]}>
+          <Heading textColor="secondary" size={4}>
+            React without Hooks
+          </Heading>
+        </Slide>
+        <Slide>
+          <CodeSlide
+            lang="js"
+            /* eslint import/no-webpack-loader-syntax: off */
+            code={require("raw-loader!./WithoutHooks")}
+            ranges={[]}
+          />
+        </Slide>
+        <Slide>
+          <WithoutHooks />
+        </Slide>
+        <Slide transition={["zoom", "fade"]}>
+          <Heading textColor="secondary" size={4}>
+            React Hooks
+          </Heading>
+        </Slide>
+        <Slide>
+          <WithHooks />
+        </Slide>
+      </Deck>
+    );
+  }
+}
