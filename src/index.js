a5c95485f269d22afcb6a36bf4fc147594688bab
zgeerts <zelia.geerts@fullsave.com>
2019-11-17 18:16:27 +0100

    Clean index

--- src/index.js
+++ src/index.js
@@ -1,35 +1,8 @@
 import React from "react";
-import PropTypes from "prop-types";
 import ReactDOM from "react-dom";
-import { AppContainer } from "react-hot-loader";
-import Redbox from "redbox-react";
 
-// import Presentation from './presentation';
 import Presentation from "./introduction";
 
-require("react-hot-loader/patch")
+require("react-hot-loader/patch");
 
-const CustomErrorReporter = ({ error }) => <Redbox error={error} />;
-
-CustomErrorReporter.propTypes = {
-  error: PropTypes.instanceOf(Error).isRequired
-};
-
-ReactDOM.render(
-  <AppContainer errorReporter={CustomErrorReporter}>
-    <Presentation />
-  </AppContainer>,
-  document.getElementById("root")
-);
-
-if (module.hot) {
-  module.hot.accept("./presentation", () => {
-    const NextPresentation = require("./presentation").default;
-    ReactDOM.render(
-      <AppContainer errorReporter={CustomErrorReporter}>
-        <NextPresentation />
-      </AppContainer>,
-      document.getElementById("root")
-    );
-  });
-}
+ReactDOM.render(<Presentation />, document.getElementById("root"));
